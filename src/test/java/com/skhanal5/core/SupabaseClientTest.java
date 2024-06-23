2b66e24c1bcda89533ac4ca2deb6228ecaf987b8
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-06-23 17:49:57 -0400

    Remove tests for removed newInstance methods

--- src/test/java/com/skhanal5/core/SupabaseClientTest.java
+++ src/test/java/com/skhanal5/core/SupabaseClientTest.java
@@ -10,39 +10,6 @@ import static org.mockito.Mockito.mock;
 public class SupabaseClientTest {
 
     @Test
-    void testNewInstanceClientOnlyMinimal() {
-        Assertions.assertThrows(NullPointerException.class, () -> SupabaseClient.newInstance(null));
-    }
-
-    @Test
-    void testNewInstanceClient() {
-        var webClient = mock(WebClient.class);
-        var supabaseClient = SupabaseClient.newInstance(webClient);
-        Assertions.assertEquals(webClient, supabaseClient.client);
-    }
-
-    @Test
-    void testNewInstanceClientAndMapperMinimal1() {
-        var mapper = mock(ObjectMapper.class);
-        Assertions.assertThrows(NullPointerException.class, () -> SupabaseClient.newInstance(null, mapper));
-    }
-
-    @Test
-    void testNewInstanceClientAndMapperMinimal2() {
-        var client = mock(WebClient.class);
-        Assertions.assertThrows(NullPointerException.class, () -> SupabaseClient.newInstance(client, null));
-    }
-
-    @Test
-    void testNewInstanceClientAndMapper() {
-        var webClient = mock(WebClient.class);
-        var mapper = mock(ObjectMapper.class);
-        var supabaseClient = SupabaseClient.newInstance(webClient, mapper);
-        Assertions.assertEquals(webClient, supabaseClient.client);
-        Assertions.assertEquals(mapper, supabaseClient.mapper);
-    }
-
-    @Test
     void testNewInstanceURLAndServiceKeyMinimal1() {
         Assertions.assertThrows(NullPointerException.class, () -> SupabaseClient.newInstance("", null));
     }
