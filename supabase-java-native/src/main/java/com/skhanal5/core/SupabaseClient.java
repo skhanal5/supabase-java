ea1c5899e53a9b9d7f67da4f3b3a4c11317392ae
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-07-13 15:39:14 -0400

    Initial implementaiton of native client

---
+++ supabase-java-native/src/main/java/com/skhanal5/core/SupabaseClient.java
@@ -0,0 +1,235 @@
+package com.skhanal5.core;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skhanal5.models.DeleteQuery;
+import com.skhanal5.models.InsertQuery;
+import com.skhanal5.models.SelectQuery;
+import com.skhanal5.models.UpdateQuery;
+import lombok.*;
+
+import java.net.http.HttpClient;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The main entry point to instantiation an instance of SupabaseClient and
+ * interact with Supabase Database via REST. Utilizes WebClient and Jackson
+ * under the hood to handle sending requests to Supabase Database API and
+ * serializing responses.
+ *
+ * We expose two ways of initializing an instance of SupabaseClient to consumers .
+ * @see #newInstance(String, String)    Using the database url and service key
+ * @see #newInstance(String, String, ObjectMapper) Using the databaes url, service key, and a Jackson ObjectMapper
+ */
+public class SupabaseClient {
+
+    @NonNull
+    HttpClient client;
+
+    @NonNull
+    ObjectMapper mapper;
+
+    private static final String ENDPOINT_PATH = "/rest/v1/";
+
+    private SupabaseClient(HttpClient client) {
+        this.client = client;
+        this.mapper = new ObjectMapper();
+    }
+
+    private SupabaseClient(HttpClient client, ObjectMapper mapper) {
+       this.client = client;
+       this.mapper = mapper;
+    }
+
+    /**
+     * Executes a SelectQuery and returns the search response as a POJO of type responseType. The responseType
+     * class definition should match the schema of your table.
+     *
+     * @param query The query to execute
+     * @param responseType The class of the POJO that you want the response to be converted to
+     * @return  the response POJO
+     * @param <T> the type of the expected response POJO
+     */
+    public <T> T executeSelect(SelectQuery query, Class<T> responseType) {
+        var queryParams = query.convertToQueryParams();
+        var additionalHeaders = query.addPaginationHeader();
+        return this.makeSelectAPICall(query.getTable(),queryParams, additionalHeaders, responseType);
+    }
+
+    /**
+     * Executes a InsertQuery and returns the inserted row as a POJO of type responseType. The responseType
+     * class definition should match the schema of your table.
+     *
+     * Note: Nothing will be returned, if select() is not explicitly invoked in your InsertQuery. The method
+     * will return null.
+     *
+     * @param query The query to execute
+     * @param responseType The class of the POJO that you want the response to be converted to
+     * @return  the response POJO or null
+     * @param <T> the type of the expected response POJO
+     */
+    public <T> T executeInsert(InsertQuery query, Class<T> responseType) {
+        var requestBody = query.getValuesToInsert();
+        var additionalHeaders = query.addSelectHeader();;
+        return this.makeInsertDBCall(query.getTable(), requestBody, additionalHeaders, responseType);
+    }
+
+    /**
+     * Executes a UpdateQuery and returns the updated row as a POJO of type responseType. The responseType
+     * class definition should match the schema of your table.
+     *
+     * Note: Nothing will be returned, if select() is not explicitly invoked in your UpdateQuery. The method
+     * will return null.
+     *
+     * @param query The query to execute
+     * @param responseType The class of the POJO that you want the response to be converted to
+     * @return  the response POJO or null
+     * @param <T> the type of the expected response POJO
+     */
+    public <T> T executeUpdate(UpdateQuery query, Class<T> responseType) {
+        var requestBody = query.getValuesToUpdate();
+        var additionalHeaders = query.addSelectHeader();
+        var queryParams = query.convertToQueryParams();
+        return this.makeUpdateDBCall(query.getTable(), additionalHeaders, queryParams, requestBody, responseType);
+    }
+
+    /**
+     * Executes a DeleteQuery and returns the deleted row as a POJO of type responseType. The responseType
+     * class definition should match the schema of your table.
+     *
+     * Note: Nothing will be returned, if select() is not explicitly invoked in your DeleteQuery. The method
+     * will return null.
+     *
+     * @param query The query to execute
+     * @param responseType The class of the POJO that you want the response to be converted to
+     * @return  the response POJO or null
+     * @param <T> the type of the expected response POJO
+     */
+    public <T> T executeDelete(DeleteQuery query, Class<T> responseType) {
+        var additionalHeaders = query.addSelectHeader();
+        var queryParams = query.convertToQueryParams();
+        return this.makeDeleteAPICall(query.getTable(), additionalHeaders, queryParams, responseType);
+    }
+
+    private <T> T makeSelectAPICall(String table,
+                                    Map<String, List<String>> queryParameters,
+                                    Map<String, List<String>> headers,
+                                    Class<T> responseType) {
+//        return client
+//                .get()
+//                .uri(uriBuilder ->  uriBuilder
+//                        .path(table)
+//                        .queryParams(queryParameters)
+//                        .build())
+//                .headers(headersConsumer)
+//                .retrieve()
+//                .bodyToMono(responseType)
+//                .block();
+    }
+
+    private <T> T makeInsertDBCall(String table,
+                                   List<Map<String, Object>> requestBody,
+                                   Map<String, List<String>> headers,
+                                   Class<T> responseType) {
+//        return client
+//                .post()
+//                .uri(uriBuilder -> {
+//                    var uri =  uriBuilder
+//                            .path(table)
+//                            .build();
+//                    System.out.println(uri);
+//                    return uri;
+//                })
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .headers(headersConsumer)
+//                .bodyValue(requestBody)
+//                .retrieve()
+//                .bodyToMono(responseType)
+//                .block();
+    }
+
+    private <T> T makeUpdateDBCall(String table,
+                                   Map<String, List<String>> headers,
+                                   Map<String, List<String>> queryParameters,
+                                   List<Map<String, Object>> requestBody,
+                                   Class<T> responseType) {
+//        return client
+//                .patch()
+//                .uri(uriBuilder -> {
+//                    var uri =  uriBuilder
+//                            .path(table)
+//                            .queryParams(queryParameters)
+//                            .build();
+//                    System.out.println(uri);
+//                    return uri;
+//                })
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .headers(headers)
+//                .bodyValue(requestBody)
+//                .retrieve()
+//                .bodyToMono(responseType)
+//                .block();
+    }
+
+    private <T> T makeDeleteAPICall(String table,
+                                    Map<String, List<String>> headers,
+                                    Map<String, List<String>> queryParameters,
+                                    Class<T> responseType) {
+//        return client
+//                .delete()
+//                .uri(uriBuilder -> {
+//                    var uri =  uriBuilder
+//                            .path(table)
+//                            .queryParams(queryParameters)
+//                            .build();
+//                    System.out.println(uri);
+//                    return uri;
+//                })
+//                .headers(headers)
+//                .retrieve()
+//                .bodyToMono(responseType)
+//                .block();
+    }
+
+    /**
+     * The most basic way of making an instance of the SupabaseClient. Utilizes built-in object mapper that
+     * this library provides with no additional configuration.
+     *
+     * @param databaseUrl Represents the Supabase Database API base URL.
+     * @param serviceKey Represents the Supabase Database service key. Note, this should not be exposed to clients.
+     * @return an instance of a SupabaseClient
+     */
+    public static SupabaseClient newInstance(@NonNull String databaseUrl, @NonNull String serviceKey) {
+        var baseUrl = databaseUrl + ENDPOINT_PATH;
+//        var client = WebClient
+//                .builder()
+//                .baseUrl(baseUrl)
+//                .defaultHeader("apikey", serviceKey)
+//                .defaultHeader("Authorization", "Bearer " + serviceKey)
+//                .build();
+        var client = null;
+        return new SupabaseClient(client);
+    }
+
+    /**
+     * An alternative factory method to building a SupabaseClient. Allows consumers to provide their own ObjectMapper
+     * in case there is a use case that requires the responseType POJO in database operations to be deserialized in a
+     * specific manner.
+     *
+     * @param databaseUrl Represents the Supabase Database API base URL.
+     * @param serviceKey Represents the Supabase Database service key. Note, this should not be exposed to clients.
+     * @param mapper An instance of Jackson's ObjectMapper.
+     * @return an instance of a SupabaseClient
+     */
+    public static SupabaseClient newInstance(@NonNull String databaseUrl, @NonNull String serviceKey, @NonNull ObjectMapper mapper) {
+        var baseUrl = databaseUrl + ENDPOINT_PATH;
+//        var client = WebClient
+//                .builder()
+//                .baseUrl(baseUrl)
+//                .defaultHeader("apikey", serviceKey)
+//                .defaultHeader("Authorization", "Bearer " + serviceKey)
+//                .build();
+
+       return new SupabaseClient(client, mapper);
+    }
+}
