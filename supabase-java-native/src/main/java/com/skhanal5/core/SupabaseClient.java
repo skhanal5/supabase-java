a127e9f2e86a8009484277fc5c69b30e46cd5c61
Subodh Khanal <subodhkhanal908@icloud.com>
2024-07-21 19:31:01 -0400

    Reduce duplicity in Native implementation (#25)
    * Clean up instance methods and tests in Spring client
    
    * Clean up native implementation, remove dupliate instance variables, isolate request logic to request class
    
    * Change access modifiers and remove internal package in native
    
    * Update readme with imports

--- supabase-java-native/src/main/java/com/skhanal5/core/SupabaseClient.java
+++ supabase-java-native/src/main/java/com/skhanal5/core/SupabaseClient.java
@@ -1,8 +1,8 @@
 package com.skhanal5.core;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.skhanal5.core.internal.SupabaseHttpRequestSender;
 import com.skhanal5.models.DeleteQuery;
 import com.skhanal5.models.InsertQuery;
 import com.skhanal5.models.SelectQuery;
@@ -26,21 +26,18 @@ import java.util.concurrent.ExecutionException;
  */
 public class SupabaseClient {
 
-    HttpClient client;
+    SupabaseHttpRequestSender sender;
 
     String baseURI;
-
-    ObjectMapper mapper;
 
     Map<String,String> defaultHeaders;
 
     private static final String ENDPOINT_PATH = "/rest/v1/";
 
     private SupabaseClient(HttpClient client, String baseURI, Map<String,String> defaultHeaders, ObjectMapper mapper) {
-        this.client = client;
+        this.sender = new SupabaseHttpRequestSender(client, mapper);
         this.baseURI = baseURI;
         this.defaultHeaders = defaultHeaders;
-        this.mapper = mapper;
     }
 
     /**
@@ -53,10 +50,10 @@ public class SupabaseClient {
      * @param <T> the type of the expected response POJO
      */
     public <T> T executeSelect(SelectQuery query, Class<T> responseType) {
-        var sender = new SupabaseHttpRequestSender<>(baseURI, client, defaultHeaders, query, responseType, mapper);
         try {
-            return sender.invokeGETRequest().get();
-        } catch (InterruptedException | ExecutionException e) {
+            var request = new SupabaseHttpRequest(baseURI, defaultHeaders, query);
+            return sender.invokeRequest("GET", request, responseType).get();
+        } catch (InterruptedException | ExecutionException | JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
@@ -74,10 +71,10 @@ public class SupabaseClient {
      * @param <T> the type of the expected response POJO
      */
     public <T> T executeInsert(InsertQuery query, Class<T> responseType) {
-        var sender = new SupabaseHttpRequestSender<>(baseURI, client, defaultHeaders, query, responseType, mapper);
         try {
-            return sender.invokePOSTRequest().get();
-        } catch (InterruptedException | ExecutionException e) {
+            var request = new SupabaseHttpRequest(baseURI, defaultHeaders, query);
+            return sender.invokeRequest("POST", request, responseType).get();
+        } catch (InterruptedException | ExecutionException | JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
@@ -95,10 +92,10 @@ public class SupabaseClient {
      * @param <T> the type of the expected response POJO
      */
     public <T> T executeUpdate(UpdateQuery query, Class<T> responseType) {
-        var sender = new SupabaseHttpRequestSender<>(baseURI, client, defaultHeaders, query, responseType, mapper);
         try {
-            return sender.invokePATCHRequest().get();
-        } catch (InterruptedException | ExecutionException e) {
+            var request = new SupabaseHttpRequest(baseURI, defaultHeaders, query);
+            return sender.invokeRequest("PATCH", request, responseType).get();
+        } catch (InterruptedException | ExecutionException | JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
@@ -116,10 +113,10 @@ public class SupabaseClient {
      * @param <T> the type of the expected response POJO
      */
     public <T> T executeDelete(DeleteQuery query, Class<T> responseType) {
-        var sender = new SupabaseHttpRequestSender<>(baseURI, client, defaultHeaders, query, responseType, mapper);
         try {
-            return sender.invokeDELETERequest().get();
-        } catch (InterruptedException | ExecutionException e) {
+            var request = new SupabaseHttpRequest(baseURI, defaultHeaders, query);
+            return sender.invokeRequest("DELETE", request, responseType).get();
+        } catch (InterruptedException | ExecutionException | JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
@@ -133,7 +130,7 @@ public class SupabaseClient {
      * @return an instance of a SupabaseClient
      */
     public static SupabaseClient newInstance(@NonNull String databaseUrl, @NonNull String serviceKey) {
-       return newInstance(databaseUrl, serviceKey, new ObjectMapper());
+        return newInstance(databaseUrl, serviceKey, new ObjectMapper());
     }
 
     /**
---
+++ supabase-java-native/src/main/java/com/skhanal5/core/SupabaseHttpRequest.java
@@ -0,0 +1,72 @@
+package com.skhanal5.core;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skhanal5.models.Query;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.util.*;
+import java.util.Map.Entry;
+
+class SupabaseHttpRequest {
+
+    URI uri;
+
+    Map<String, String> queryParameters;
+
+    List<Map<String, Object>> requestBody;
+
+    Map<String, String> headers;
+
+    private static final ObjectMapper requestMapper = new ObjectMapper();
+
+    SupabaseHttpRequest(String baseURI,
+                               Map<String,String> defaultHeaders,
+                               Query query) {
+        this.queryParameters = query.buildQueryParams().orElse(Map.of());
+        this.uri = buildURI(baseURI, query.getTable(), queryParameters);
+        this.requestBody = query.buildRequestBody().orElse(List.of());
+        this.headers = mergeHeaders(defaultHeaders, query.buildAdditionalHeaders());
+    }
+
+    private Map<String,String> mergeHeaders(Map<String,String> headers, Optional<Map<String,String>> headersToAdd) {
+        var mergedHeaders = new HashMap<>(headers);
+        headersToAdd.ifPresent(mergedHeaders::putAll);
+        return mergedHeaders;
+    }
+    HttpRequest buildRequest(String methodName) throws JsonProcessingException {
+        var requestBuilder = HttpRequest.newBuilder();
+        headers.forEach(requestBuilder::setHeader);
+        headers.put("Content-Type", "application/json"); //move this inside of the query's buildHeaders method as needed
+        String requestBodyToJsonString = requestMapper.writeValueAsString(requestBody);
+        return requestBuilder
+                .uri(uri)
+                .method(methodName,  HttpRequest.BodyPublishers.ofString(requestBodyToJsonString))
+                .build();
+    }
+
+    private static URI buildURI(String baseURI, String path, Map<String, String> queryParameters) {
+        String fullURI = baseURI + path + "?" + serializeQueryParameters(queryParameters);
+        return URI.create(fullURI);
+    }
+
+    private static String serializeQueryParameters(Map<String, String> queryParameters) {
+        if (queryParameters == null || queryParameters.isEmpty()) {
+            return "";
+        }
+
+        StringJoiner stringifyPathParams = new StringJoiner("&");
+        for (Entry<String, String> keyValuePair : queryParameters.entrySet()) {
+            String currPathParam = keyValuePair.getKey() + "=" + encodeSpaces(keyValuePair.getValue());
+            stringifyPathParams
+                    .add(currPathParam);
+        }
+        return stringifyPathParams.toString();
+    }
+
+    private static String encodeSpaces(String URI) {
+        return URI.replace(" ", "%20");
+    }
+
+}
