2fe2acbbadcba359b25c45712b2a56f48c747ca9
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-10-06 19:32:33 -0400

    Adding ClientIntegrationTest file with partial impl

--- supabase-java-spring/src/test/java/com/skhanal5/core/ClientIntegrationTest.java
+++ supabase-java-spring/src/test/java/com/skhanal5/core/ClientIntegrationTest.java
@@ -1,2 +1,23 @@
-package com.skhanal5.core;public class ClientIntegrationTest {
+package com.skhanal5.core;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+@WireMockTest
+public class ClientIntegrationTest {
+
+    @Test
+    void testExecuteSelect() {
+
+        stubFor(WireMock.get(urlEqualTo("")).willReturn(aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody("")));
+
+        var client = SupabaseClient.newInstance("http://localhost:8080","");
+
+    }
+
 }
